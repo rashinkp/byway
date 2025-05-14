6b498d7b77833124174ddad1ea2b0fcbe68ee161
Admin <rashinkp001@gmail.com>
2025-05-14 09:50:38 +0530

    loading state added while rendering

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,3 +1,4 @@
+// app/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -5,6 +6,8 @@ import { Toaster } from "sonner";
 import { Providers } from "@/components/providers";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import BywayFooter from "@/components/Footer";
+import { LoadingProvider } from "@/lib/context/LoadingContext";
+import { Loading } from "@/components/GlobalLoading";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,15 +34,18 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
-        <Providers>
-          <GoogleOAuthProvider
-            clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}
-          >
-            <div className="flex-1">{children}</div>
-            <BywayFooter />
-          </GoogleOAuthProvider>
-        </Providers>
-        <Toaster richColors position="top-right" />
+        <LoadingProvider>
+          <Providers>
+            <GoogleOAuthProvider
+              clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}
+            >
+              <Loading />
+              <div className="flex-1">{children}</div>
+              <BywayFooter />
+            </GoogleOAuthProvider>
+          </Providers>
+          <Toaster richColors position="top-right" />
+        </LoadingProvider>
       </body>
     </html>
   );
