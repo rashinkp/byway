ea750f5d3b9dd4791859e4d16dd4e11235391e65
Admin <rashinkp001@gmail.com>
2025-07-15 11:23:34 +0530

    updated cofig files

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["res.cloudinary.com", "platform-lookaside.fbsbx.com", "by-way-uploads.s3.ap-south-1.amazonaws.com", "byway-uploads.s3.amazonaws.com" ,"byway.ddns.net"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/dxogdfuse/image/upload/**',
+      },
+      // Add other remote patterns as needed
+    ],
+    domains: [
+      "res.cloudinary.com",
+      "platform-lookaside.fbsbx.com",
+      "by-way-uploads.s3.ap-south-1.amazonaws.com",
+      "byway-uploads.s3.amazonaws.com",
+      "byway.ddns.net"
+    ],
   },
 };
 
