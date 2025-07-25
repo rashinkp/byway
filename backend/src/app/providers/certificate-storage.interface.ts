b7fbdc088f9226088907c3d2a15e1a095655b741
Admin <rashinkp001@gmail.com>
2025-07-25 18:44:24 +0530

    updated aws env files

--- backend/src/app/providers/certificate-storage.interface.ts
+++ backend/src/app/providers/certificate-storage.interface.ts
@@ -1,4 +1,4 @@
 export interface CertificateStorageServiceInterface {
   uploadCertificate(pdfBuffer: Buffer, certificateNumber: string): Promise<string>;
-  deleteCertificate(certificateUrl: string): Promise<void>;
+  deleteCertificate(certificateUrl: string): Promise<void>; 
 } 
