b951a555ae69f41c26938e979a21cd67ee02c8c2
Admin <rashinkp001@gmail.com>
2025-07-25 15:30:55 +0530

    updated required things to make it into production

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -1,22 +1,22 @@
 import io from "socket.io-client";
 
 
-export const getToken = () => {
-	if (typeof window !== "undefined") {
-		return document.cookie
-			.split("; ")
-			.find((row) => row.startsWith("access_token="))
-			?.split("=")[1];
-	}
-	return undefined;
-};
+// export const getToken = () => {
+// 	if (typeof window !== "undefined") {
+// 		return document.cookie
+// 			.split("; ")
+// 			.find((row) => row.startsWith("access_token="))
+// 			?.split("=")[1];
+// 	}
+// 	return undefined;
+// };
 
 // Create socket connection - cookies will be sent automatically by the browser
 const socket: any = io(process.env.NEXT_PUBLIC_SOCKET_URL || "", {
 	autoConnect: false,
-	auth: {
-		token: getToken(),
-	},
+	// auth: {
+	// 	token: getToken(),
+	// },
 });
 socket.on("connect", () => {
 	console.log("[Socket] Connected:", socket.id);
